783b5984151ed71c86cd4f55d70defa1d0ecd044
dentonlt <2455296+dentonlt@users.noreply.github.com>
2024-12-22 22:50:44 -0600

    add doc interaction
    send text to Word document

--- src/index.js
+++ src/index.js
@@ -2,18 +2,48 @@
 //
 // minimal Word Addin
 //
-// minimal index.js file for use with 'webpack serve' to localhost:3000.
+// small-ish file for use with 'webpack serve' to localhost:3000.
 // webpack injects the code below into dist/taskpane.html, then serves
 // that file.
 // 
 // CHANGES
-// 20241221 dentonlt - respectfully taken/modified the webpack 5 Getting Started:
+// 20241221 dentonlt
+//      respectfully taken/modified the webpack 5 Getting Started:
 //      https://webpack.js.org/guides/getting-started/
+//      and the MS Office Add-in Sample for Word:
+//      https://github.com/OfficeDev/Office-Add-in-samples/blob/main/Samples/hello-world/word-hello-world/taskpane.html
+  
+/*eslint max-len: ["error", { "code": 80 }]*/
 
+////////// Silly inject example /////////////
 function component() {
-  const element = document.createElement('div');
-  element.innerHTML = "Yo! Cool Add-in, yo!";
+    const element = document.createElement('div');
+    element.innerHTML = "Yo! Cool Add-in, yo!";
 
-  return element;
-}
-document.body.appendChild(component());
+    return element;
+  }
+  document.body.appendChild(component());
+    
+//// Actual document interaction code //////////////
+
+// appease the error checker:
+/* global document, Office, Word */
+
+  Office.onReady((info) => {
+      // Check that we loaded into Word
+      if (info.host === Office.HostType.Word) {
+          document.getElementById("doButton").onclick = callbackDoButton;
+      }
+  });
+  
+  function callbackDoButton() {
+      return Word.run((context) => {
+  
+          // insert a paragraph at the start of the document.
+          const paragraph = context.document.body.insertParagraph("What a mess ...", Word.InsertLocation.start);
+          
+          // sync the context to run the previous API call, and return.
+          return context.sync();
+      });
+  }
+  
